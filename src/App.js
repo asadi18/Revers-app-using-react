29772ad5fcb2f2a68ce7a10d1f9cdd5892b48f64
asadi18 <asadishteaque@gmail.com>
2023-05-27 15:46:56 +0600

    Reverse app using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
+const ToDo = (props) => (
+  <tr>
+    <td>
+      <label>{props.id}</label>
+    </td>
+    <td>
+      <input />
+    </td>
+    <td>
+      <label>{props.createdAt}</label>
+    </td>
+  </tr>
+);
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: "todo1",
+      createdAt: "18.00",
+    },
+    {
+      id: "todo2",
+      createdAt: "20.30",
+    },
+    
+  ]);
+
+  const reverseOrder = () => {
+    setTodos([...todos].reverse());
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={reverseOrder}>Reverse</button>
+      <table>
+        <tbody>
+          {todos.map((todo, index) => (
+            <ToDo key={index} id={todo.id} createdAt={todo.createdAt} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
